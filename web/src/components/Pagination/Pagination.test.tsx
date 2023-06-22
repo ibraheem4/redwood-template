82dc09e42c091b713ed8b8c32922c45859a7850c
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2023-06-22 10:43:32 -0400

    test: add edge cases for pagination

--- web/src/components/Pagination/Pagination.test.tsx
+++ web/src/components/Pagination/Pagination.test.tsx
@@ -1,8 +1,21 @@
+import { useLocation } from '@redwoodjs/router'
 import { render } from '@redwoodjs/testing'
 
 import Pagination from './Pagination'
 
+jest.mock('@redwoodjs/router', () => ({
+  ...jest.requireActual('@redwoodjs/router'),
+  useLocation: jest.fn(),
+}))
+
+// Cast 'useLocation' as a jest mock function
+const mockUseLocation = useLocation as jest.Mock
+
 describe('Pagination', () => {
+  beforeEach(() => {
+    mockUseLocation.mockImplementation(() => ({ search: '?page=1' }))
+  })
+
   it('renders the correct number of pagination items', () => {
     const count = 10
     const postsPerPage = 5
@@ -12,9 +25,8 @@ describe('Pagination', () => {
     )
 
     const paginationItems = getAllByRole('link')
-    expect(paginationItems).toHaveLength(2) // Since count is 10 and postsPerPage is 5, there should be 2 pagination items
+    expect(paginationItems).toHaveLength(2)
 
-    // Verify the text content and href of each pagination item
     paginationItems.forEach((item, index) => {
       const pageNumber = index + 1
       expect(item).toHaveTextContent(String(pageNumber))
@@ -31,7 +43,7 @@ describe('Pagination', () => {
     )
 
     const currentPage = getByText('1')
-    expect(currentPage).toHaveClass('bg-blue-500 text-white') // The first page should have the active styling
+    expect(currentPage).toHaveClass('bg-blue-500 text-white')
   })
 
   it('renders the non-current pages with the correct styling', () => {
@@ -43,6 +55,73 @@ describe('Pagination', () => {
     )
 
     const nonCurrentPage = getByText('2')
-    expect(nonCurrentPage).toHaveClass('bg-blue-100 text-blue-700') // The second page should have the non-active styling
+    expect(nonCurrentPage).toHaveClass('bg-blue-100 text-blue-700')
+  })
+
+  it('renders with just enough posts to create a new page', () => {
+    const count = 10
+    const postsPerPage = 5
+
+    mockUseLocation.mockImplementation(() => ({ search: '?page=2' }))
+
+    const { getByText } = render(
+      <Pagination count={count} postsPerPage={postsPerPage} />
+    )
+
+    const currentPage = getByText('2')
+    expect(currentPage).toHaveClass('bg-blue-500 text-white')
+  })
+
+  it('renders correctly with no posts', () => {
+    const count = 0
+    const postsPerPage = 5
+
+    const { queryByRole } = render(
+      <Pagination count={count} postsPerPage={postsPerPage} />
+    )
+
+    expect(queryByRole('link')).not.toBeInTheDocument()
+  })
+
+  it('renders the correct number of pagination items when count is not evenly divisible by postsPerPage', () => {
+    const count = 17
+    const postsPerPage = 5
+
+    mockUseLocation.mockImplementation(() => ({ search: '?page=1' }))
+
+    const { getAllByRole } = render(
+      <Pagination count={count} postsPerPage={postsPerPage} />
+    )
+
+    const paginationItems = getAllByRole('link')
+    expect(paginationItems).toHaveLength(4)
+
+    paginationItems.forEach((item, index) => {
+      const pageNumber = index + 1
+      expect(item).toHaveTextContent(String(pageNumber))
+      expect(item).toHaveAttribute('href', `/?page=${pageNumber}`)
+    })
+  })
+
+  it('renders the correct number of pagination items when postsPerPage is greater than count', () => {
+    const count = 5
+    const postsPerPage = 10
+
+    mockUseLocation.mockImplementation(() => ({ search: '?page=1' }))
+
+    const { getAllByRole } = render(
+      <Pagination count={count} postsPerPage={postsPerPage} />
+    )
+
+    const paginationItems = getAllByRole('link')
+    // eslint-disable-next-line jest-dom/prefer-in-document
+    expect(paginationItems).toHaveLength(1) // verify that only one pagination item exists
+
+    paginationItems.forEach((item, index) => {
+      const pageNumber = index + 1
+      expect(item).toBeInTheDocument() // use .toBeInTheDocument() to assert the existence of each item
+      expect(item).toHaveTextContent(String(pageNumber))
+      expect(item).toHaveAttribute('href', `/?page=${pageNumber}`)
+    })
   })
 })
