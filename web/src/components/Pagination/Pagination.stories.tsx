a1755048385c0fee92c46b8b8ed20cfa8985065d
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2023-06-22 04:11:50 -0400

    test: add stories for pagination

--- web/src/components/Pagination/Pagination.stories.tsx
+++ web/src/components/Pagination/Pagination.stories.tsx
@@ -1,9 +1,23 @@
-import type { ComponentMeta } from '@storybook/react'
+import type { ComponentMeta, ComponentStory } from '@storybook/react'
 
 import Pagination from './Pagination'
 
-export const generated = (args) => {
+export const Default: ComponentStory<typeof Pagination> = (args) => {
   return <Pagination {...args} />
+}
+
+Default.args = {
+  count: 30,
+  postsPerPage: 5,
+}
+
+export const MorePages: ComponentStory<typeof Pagination> = (args) => {
+  return <Pagination {...args} />
+}
+
+MorePages.args = {
+  count: 100,
+  postsPerPage: 5,
 }
 
 export default {
