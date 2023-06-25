10f49875aafed7adb0fb7b78dc5e8974ba04f7dc
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2023-06-25 12:28:57 -0400

    refactor: modernize route structure

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -9,26 +9,24 @@ import { useAuth } from './auth'
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
-      <Set wrap={AuthLayout}>
-        <Route path="/login" page={LoginPage} name="login" />
-        <Route path="/signup" page={SignupPage} name="signup" />
-        <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
-        <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
-      </Set>
       <Set wrap={BlogLayout}>
         <Route path="/blog-post/{id:String}" page={BlogPostPage} name="blogPost" />
         <Route path="/contact" page={ContactPage} name="contact" />
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
       </Set>
-      <Private unauthenticated="home" roles={['admin']}>
-        <Set wrap={AdminLayout}>
-          <Route path="/admin/posts/new" page={PostNewPostPage} name="newPost" />
-          <Route path="/admin/posts/{id:String}/edit" page={PostEditPostPage} name="editPost" />
-          <Route path="/admin/posts/{id:String}" page={PostPostPage} name="post" />
-          <Route path="/admin/posts" page={PostPostsPage} name="posts" />
-        </Set>
-      </Private>
+      <Set wrap={AuthLayout}>
+        <Route path="/login" page={LoginPage} name="login" />
+        <Route path="/signup" page={SignupPage} name="signup" />
+        <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+        <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
+      </Set>
+      <Set wrap={AdminLayout} private unauthenticated="home" roles={['admin']}>
+        <Route path="/admin/posts/new" page={PostNewPostPage} name="newPost" />
+        <Route path="/admin/posts/{id:String}/edit" page={PostEditPostPage} name="editPost" />
+        <Route path="/admin/posts/{id:String}" page={PostPostPage} name="post" />
+        <Route path="/admin/posts" page={PostPostsPage} name="posts" />
+      </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
