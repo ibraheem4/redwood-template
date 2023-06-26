03ca8a0d4ea569e3f3012469ad93f6dd2b85b9ee
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2023-06-26 00:17:17 -0400

    refactor: improve routeUtils

--- web/src/utils/routeUtils.ts
+++ web/src/utils/routeUtils.ts
@@ -1,20 +1,23 @@
 import { useLocation } from '@redwoodjs/router'
 
+import { DEFAULT_LANGUAGE } from 'src/utils/constants'
+
 export const appendLangToRoute = (route: string): string => {
   const { search } = useLocation()
   const params = new URLSearchParams(search)
   const langParam = params.get('lang')
 
-  if (langParam) {
-    // Check if route already has a query param
-    if (route.includes('?')) {
-      // Use & to concatenate if route already has a param
-      return `${route}&lang=${langParam}`
-    } else {
-      // Else, use ? to start the params in the route
-      return `${route}?lang=${langParam}`
-    }
-  } else {
+  // If langParam is null or equal to DEFAULT_LANGUAGE, we don't append anything
+  if (!langParam || langParam === DEFAULT_LANGUAGE) {
     return route
+  }
+
+  // Check if route already has a query param
+  if (route.includes('?')) {
+    // Use & to concatenate if route already has a param
+    return `${route}&lang=${langParam}`
+  } else {
+    // Else, use ? to start the params in the route
+    return `${route}?lang=${langParam}`
   }
 }
