df1ded3c3975cfec4db3a39c3ceee93dce9c1ce2
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2023-07-12 20:53:42 -0400

    fix: language select

--- web/src/utils/translations.ts
+++ web/src/utils/translations.ts
@@ -4,17 +4,18 @@ import { useTranslation } from 'react-i18next'
 const useLanguageDirection = () => {
   const { t, i18n } = useTranslation()
 
-  const directionValue = Object.freeze(['ar']).includes(i18n.language)
-    ? 'rtl'
-    : 'ltr'
-
   const changeLang = (newLang) => {
     i18n.changeLanguage(newLang)
+
+    const directionValue = Object.freeze(['ar']).includes(newLang)
+      ? 'rtl'
+      : 'ltr'
+
     document.documentElement.setAttribute('dir', directionValue)
     document.documentElement.lang = newLang
   }
 
-  return { t, i18n, directionValue, changeLang }
+  return { t, i18n, changeLang }
 }
 
 export { useLanguageDirection }
