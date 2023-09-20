ad7b503c5ddb91593705f1345674ed948f94ebf9
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2023-09-20 07:13:59 -0400

    build: use vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,14 +1,17 @@
 import dns from 'dns'
 
-import type { UserConfig } from 'vite'
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
+
+import redwood from '@redwoodjs/vite'
 
 // See: https://vitejs.dev/config/server-options.html#server-host
 // So that Vite will load on local instead of 127.0.0.1
 dns.setDefaultResultOrder('verbatim')
 
-import redwood from '@redwoodjs/vite'
-
+/**
+ * https://vitejs.dev/config/
+ * @type {import('vite').UserConfig}
+ */
 const viteConfig: UserConfig = {
   plugins: [redwood()],
 }
