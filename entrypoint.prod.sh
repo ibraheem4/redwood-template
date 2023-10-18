69534c6d6573e724a60ff10fee5e7e72ac7e7cf7
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2023-10-18 08:48:24 -0400

    refactor: remove prod files

--- entrypoint.prod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd /app # Navigate to the correct directory
-
-# Run Prisma migrations (if needed in production)
-yarn rw prisma migrate deploy
-
-# Start the Redwood API server
-exec yarn rw serve api
