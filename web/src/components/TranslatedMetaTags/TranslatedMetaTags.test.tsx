d4e9ef918068fbc596fa63349be0e11a233906d1
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2024-03-20 08:56:21 -0400

    test: fix import

--- web/src/components/TranslatedMetaTags/TranslatedMetaTags.test.tsx
+++ web/src/components/TranslatedMetaTags/TranslatedMetaTags.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { render, waitFor } from '@testing-library/react'
 
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom'
 import { MetaTags as RedwoodMetaTags } from '@redwoodjs/web'
 
 import { useLanguageDirection } from 'src/utils/translations'
