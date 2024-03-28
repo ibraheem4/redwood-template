46144e43cae7ab3b8c972f3f91838fc087f30105
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2024-03-28 02:46:10 -0400

    build: reduce dockerfile layers
    - debug DATABASE_URL
    - seed test environment

--- entrypoint.local.sh
+++ entrypoint.local.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 # Set DATABASE_URL from DB_SECRET if available
+echo "DB_SECRET: $DB_SECRET"
 if [ -n "$DB_SECRET" ]; then
   echo "Using DB_SECRET to set DATABASE_URL..."
   export DATABASE_URL=$(echo $DB_SECRET | jq -r '"postgresql://" + .username + ":" + .password + "@" + .host + ":" + .port + "/" + .dbname')
+  echo "DATABASE_URL: $DATABASE_URL"
+else
+  echo "DB_SECRET not found"
 fi
 
 # Your existing script content
@@ -14,11 +18,11 @@ echo "Running Prisma migrations..."
 yarn rw prisma migrate dev
 
 # Seed the database
-if [ "$COPILOT_ENVIRONMENT_NAME" = "development" ]; then
-  echo "Environment is development. Seeding the database..."
+if [[ "$COPILOT_ENVIRONMENT_NAME" == "development" || "$COPILOT_ENVIRONMENT_NAME" == "test" ]]; then
+  echo "Environment is $COPILOT_ENVIRONMENT_NAME. Seeding the database..."
   yarn rw prisma db seed
 else
-  echo "Environment is not set to development. Skipping database seeding."
+  echo "Environment is not set to development or test. Skipping database seeding."
 fi
 
 # Start the Redwood API server
